3c609b94296085e6eda9de70253fae72caf81806
Antonioescp <packageinstaller2@gmail.com>
2023-06-29 01:00:22 -0600

    Settings: JSON Serializer ReferenceHandler will ignore cyclical references

--- AppDeipesaAPI/Program.cs
+++ AppDeipesaAPI/Program.cs
@@ -1,12 +1,18 @@
 global using AppDeipesaAPI.Models;
 
 global using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
